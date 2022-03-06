1e20fd9336767c7cd04f03be3626a721f41a49c4
Minjae Chang <minjaejchang@gmail.com>
2022-03-06 19:38:24 +1300

    Add state and onClick functions

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import styles from './Habit.module.css';
 
 class Habit extends Component {
+  state = { count: 0 };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
     return (
       <li className={styles.habit}>
         <span className={styles.habit__name}>Coding</span>
-        <span className={styles.habit__count}>9</span>
-        <button className={`${styles.habit__button} ${styles.habit__increase}`}>
+        <span className={styles.habit__count}>{this.state.count}</span>
+        <button
+          className={`${styles.habit__button} ${styles.habit__increase}`}
+          onClick={this.handleIncrement}
+        >
           <i class='fa-solid fa-square-plus'></i>
         </button>
-        <button className={`${styles.habit__button} ${styles.habit__decrease}`}>
+        <button
+          className={`${styles.habit__button} ${styles.habit__decrease}`}
+          onClick={this.handleDecrement}
+        >
           <i class='fa-solid fa-square-minus'></i>
         </button>
-        <button className={`${styles.habit__button} ${styles.habit__delete}`}>
+        <button
+          className={`${styles.habit__button} ${styles.habit__delete}`}
+          onClick={this.handleDelete}
+        >
           <i class='fa-solid fa-trash'></i>
         </button>
       </li>
