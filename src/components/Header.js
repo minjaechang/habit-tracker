0fdf9070045d96e3b8b7f9ef6b545199bcd0c687
Minjae Chang <minjaejchang@gmail.com>
2022-03-07 21:07:02 +1300

    Add design for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import styles from './Header.module.css';
 
 class Header extends Component {
   render() {
     return (
-      <header>
-        <span className='header__title'>Habit Tracker</span>
-        <span className='header__count'>{this.props.totalCount}</span>
+      <header className={styles.header}>
+        <span className={styles.header__title}>Habit Tracker</span>
+        <span className={styles.header__count}>{this.props.totalCount}</span>
       </header>
     );
   }
