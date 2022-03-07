4f05702b2532b151b261cdbd1268e1f1fe0ab255
Minjae Chang <minjaejchang@gmail.com>
2022-03-07 18:34:25 +1300

    Update App component to include state and functions

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,62 @@
 import './app.css';
 import HabitAddForm from './components/HabitAddForm';
 import Habits from './components/Habits';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <>
-      <HabitAddForm />
-      <Habits />
-    </>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Coding', count: 0 },
+      { id: 2, name: 'Reading', count: 0 },
+      { id: 3, name: 'Exercise', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    // TODO: Check if the function works
+    const habits = this.state.habits.filter((item) => {
+      return item.id !== habit.id;
+    });
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <HabitAddForm />
+        <Habits
+          habits={this.state.habits}
+          handleIncrement={this.handleIncrement}
+          handleDecrement={this.handleDecrement}
+          handleDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Habits.js
+++ src/components/Habits.js
@@ -2,51 +2,14 @@ import React, { Component } from 'react';
 import Habit from './Habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Coding', count: 0 },
-      { id: 2, name: 'Reading', count: 0 },
-      { id: 3, name: 'Exercise', count: 0 },
-    ],
-  };
-
-  handleIncrement = (habit) => {
-    const habits = this.state.habits.map((item) => {
-      if (item.id === habit.id) {
-        return { ...habit, count: habit.count + 1 };
-      }
-      return item;
-    });
-    this.setState({ habits });
-  };
-
-  handleDecrement = (habit) => {
-    const habits = this.state.habits.map((item) => {
-      if (item.id === habit.id) {
-        const count = habit.count - 1;
-        return { ...habit, count: count < 0 ? 0 : count };
-      }
-      return item;
-    });
-    this.setState({ habits });
-  };
-
-  handleDelete = (habit) => {
-    // TODO: Check if the function works
-    const habits = this.state.habits.filter((item) => {
-      return item.id !== habit.id;
-    });
-    this.setState({ habits });
-  };
-
   render() {
-    return this.state.habits.map((habit) => (
+    return this.props.habits.map((habit) => (
       <Habit
         key={habit.id}
         habit={habit}
-        onIncrement={this.handleIncrement}
-        onDecrement={this.handleDecrement}
-        onDelete={this.handleDelete}
+        onIncrement={this.props.handleIncrement}
+        onDecrement={this.props.handleDecrement}
+        onDelete={this.props.handleDelete}
       />
     ));
   }
